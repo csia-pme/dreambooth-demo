98caad77de305f756b6f3b99be3d6797fa34675b
Adrien Allemand <adrien.allemand@heig-vd.ch>
2023-02-24 09:59:50 +0100

    fix broken infere.py state

--- scripts/infere.py
+++ scripts/infere.py
@@ -7,17 +7,17 @@ print('Starting inference...')
 model_id = '../model/' + os.environ.get('SUBJECT_NAME')
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to('cuda')
 
-style = ', beautiful face, symmetrical, centered, dramatic angle, ornate, details, smooth, sharp focus, illustration, realistic, cinematic, artstation, award winning, rgb , unreal engine, octane render, cinematic light, macro, depth of field, blur'
+style = ' , beautiful face, symmetrical, centered, dramatic angle, ornate, details, smooth, sharp focus, illustration, realistic, cinematic, 8k, award winning, rgb , unreal engine, octane render, cinematic light, depth of field, blur'
 subjectName = os.environ.get('SUBJECT_NAME')
-subjectGender = os.environ.get('SUBJECT_GENDER', '')
+subjectGender = ' ' + os.environ.get('SUBJECT_GENDER', '')
 subjects = [
-    'a portrait of ' + subjectName + ' ' , 
-    'a portrait of ' + subjectName + ' as a medieval knight ', 
-    'a portrait of ' + subjectName + ' as Ironman ',
+    'a portrait of ' + subjectName + '' , 
+    'a portrait of ' + subjectName + ' as a medieval knight', 
+    'a portrait of ' + subjectName + ' as Ironman wihout helmet',
     'a portrait of ' + subjectName + ' as a medieval swiss person',
-    'a portrait of ' + subjectName + ' as a 17 century noble ',
-    'a portrait of ' + subjectName + ' as president of the USA ',
-    'a portrait of ' + subjectName + ' as an elf ',
+    'a portrait of ' + subjectName + ' as a 17 century noble',
+    'a portrait of ' + subjectName + ' as president of the USA',
+    'a portrait of ' + subjectName + ' as an elf',
     ]
 
 for subject in subjects:
@@ -26,10 +26,7 @@ for subject in subjects:
     print('Inference of ' + image_name)
     prompt = subject + subjectGender + style 
     image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
-
     image.save('../images/' + os.environ.get('SUBJECT_NAME') + '/' + image_name)
-
-
 
     print('Image generated ' + image_name)
 
--- scripts/train.sh
+++ scripts/train.sh
@@ -21,7 +21,9 @@ export CLASS_DIR="../class"
   accelerate launch ./diffusers/examples/dreambooth/train_dreambooth.py \
   --pretrained_model_name_or_path=$MODEL_NAME  \
   --instance_data_dir=$INSTANCE_DIR \
+  --class_data_dir=$CLASS_DIR \
   --output_dir=$OUTPUT_DIR \
+  --with_prior_preservation --prior_loss_weight=1.0 \
   --instance_prompt="a photo of $SUBJECT_NAME the person" \
   --class_prompt="a photo of a person" \
   --resolution=512 \
@@ -30,9 +32,6 @@ export CLASS_DIR="../class"
   --learning_rate=5e-6 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --max_train_steps=800 \
-  --with_prior_preservation --prior_loss_weight=1.0 \
-  --class_data_dir=$CLASS_DIR \
   --num_class_images=100 \
-  --checkpointing_steps=100
+  --max_train_steps=800
 fi
