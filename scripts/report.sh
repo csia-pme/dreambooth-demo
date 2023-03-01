b063dc78ebcf7a92479ead94d9861b57b459a823
Adrien Allemand <adrien.allemand@heig-vd.ch>
2023-03-01 13:19:43 +0100

    fix report script to integrate checkpoint generated images

--- scripts/report.sh
+++ scripts/report.sh
@@ -1,3 +1,5 @@
+#!bin/bash
+
 echo "# Inference report for $SUBJECT_NAME" >> report.md
 
 echo "ls ./images/$SUBJECT_NAME"
@@ -8,10 +10,9 @@ FILES="./images/$SUBJECT_NAME/*/"
 # we have one directory for each prompt :
 for d in $FILES ; do
     echo "## $d" >> report.md
-    for f in $d
-    do
-    # take action on each file. $f store current file name
-    echo "![]($f)" >> report.md
+    for f in $d ; do
+        # take action on each file. $f store current file name
+        echo "![]($f)" >> report.md
     done
 done
 
