6dc1c3f37acde1e637fe3397a38cb35cb08f5a46
Adrien Allemand <adrien.allemand@heig-vd.ch>
2023-03-13 11:30:08 +0100

    specify seed in generator for inference

--- scripts/infere.py
+++ scripts/infere.py
@@ -5,7 +5,7 @@ import os
 import yaml
 
 def make_image_grid(imgs, rows, cols):
-    assert len(imgs) >= rows*cols
+    assert len(imgs) == rows*cols
 
     w, h = imgs[0].size
     grid = Image.new('RGB', size=(cols*w, rows*h))
@@ -23,11 +23,12 @@ output_path = './images'
 if not os.path.exists(output_path) :
     os.makedirs(output_path)
 
-pipe = StableDiffusionPipeline.from_pretrained(model_path, torch_dtype=torch.float16).to('cuda')
+generator = torch.Generator("cuda").manual_seed(params['infere'])
+pipe = StableDiffusionPipeline.from_pretrained(model_path, torch_dtype=torch.float16)
 
 prompts = [params['prompt']] * params['number_images']
 
-images = pipe(prompts , num_inference_steps=params['steps'], guidance_scale=params['guidance'])
+images = pipe(prompts, generator=generator, num_inference_steps=params['steps'], guidance_scale=params['guidance']).images
 
 image_grid = make_image_grid(images, 1, params['number_images'])
    
