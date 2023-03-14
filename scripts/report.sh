5161da8b928764b1c85b0d62c8cf126f50c0dcd9
Adrien Allemand <adrien.allemand@heig-vd.ch>
2023-03-14 13:43:33 +0100

    adapt pipeline to dvc usage

--- scripts/report.sh
+++ scripts/report.sh
@@ -1,17 +1,12 @@
 #!bin/bash
 
-echo "ls ./images/$SUBJECT_NAME"
-ls "./images/$SUBJECT_NAME"
+rm -f report.md
+echo "# Experiment reproduction report" >> report.md
+echo "## Parameters used" >> report.md
+echo "\`\`\`yaml" >> report.md
+cat params.yaml >> report.md
+echo "\`\`\`" >> report.md
+echo "## Results" >> report.md
+echo "![](./images/grid.jpg)" >> report.md
+cml comment create --target=commit --publish report.md
 
-FILES="./images/$SUBJECT_NAME/*/"
-
-# we have one directory for each prompt :
-for d in $FILES ; do
-    rm -f report.md
-    echo "# $d" >> report.md
-    for f in $d ; do
-        # take action on each file. $f store current file name
-        echo "![]($f)" >> report.md
-    done
-    cml comment create --target=commit --publish report.md
-done
